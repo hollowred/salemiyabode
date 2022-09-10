ba95d7a68312bce49f0c7bbc0baf413dc40a70a6
Salem Iyabode <Shelton@Sheltons-MacBook-Pro.local>
2022-09-10 18:18:57 -0500

    headersocials

--- react-portfolio/src/components/header/Header.jsx
+++ react-portfolio/src/components/header/Header.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
+import ME from '../../assets/me.png'
+import HeaderSocial from './HeaderSocials'
 
 const Header = () => {
   return (
@@ -10,6 +12,13 @@ const Header = () => {
         <h1>Salem Flagg</h1>
         <h5 className="text-light">Fullstack Developer</h5> 
         <CTA />
+        <HeaderSocial />
+
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>  
+
+        <a href="#contact" className='scroll_down'>Scroll Down</a>
      </div>
     </header>
   )
