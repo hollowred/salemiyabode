9ae836fd6f55a71acfd2cbe36c0830290594e4f0
Salem Iyabode <Shelton@Sheltons-MacBook-Pro.local>
2022-09-11 14:06:27 -0500

    contact component started

--- react-portfolio/src/components/contact/contact.jsx
+++ react-portfolio/src/components/contact/contact.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 import './contact.css'
+import {AiOutlineMail} from 'react-icons/ai'
 
 const Contact = () => {
   return (
-    <section id="contact">Contact</section>
+    <section id="contact">
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <AiOutlineMail />
+            <h4>Email</h4>
+            <h5>salemiyabode@gmail.com</h5>
+            <a href="mailto:salemiyabode@gmail.com">Send a message</a>
+          </article>  
+
+        </div> 
+        {/* END OF CONTACT OPTIONS   */}
+        <form action=""></form>
+      </div>  
+
+
+    </section>
   )
 }
 
