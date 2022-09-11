5c7d21f4ccbef9e2d3ba0ee0e640d629204edc11
Salem Iyabode <Shelton@Sheltons-MacBook-Pro.local>
2022-09-11 08:43:00 -0500

    completed nav component

--- react-portfolio/src/components/nav/Nav.jsx
+++ react-portfolio/src/components/nav/Nav.jsx
@@ -13,9 +13,9 @@ const Nav = () => {
     <nav>
       <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiFillHome /></a>
       <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser /></a>
-      <a href="#experience"><BiBookAlt /></a>
-      <a href="#portfolio"><FaSuitcase /></a>
-      <a href="#contact"><BiMessageDots/></a>
+      <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BiBookAlt /></a>
+      <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><FaSuitcase /></a>
+      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiMessageDots/></a>
     </nav>  
   )
 }
