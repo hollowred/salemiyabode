ad90cf2c5e0b17e5feae109ca357ea375d2fec15
Salem Iyabode <Shelton@Sheltons-MacBook-Pro.local>
2022-09-11 15:59:07 -0500

    edits to contact css

--- react-portfolio/src/components/header/HeaderSocials.jsx
+++ react-portfolio/src/components/header/HeaderSocials.jsx
@@ -5,8 +5,8 @@ import { FaGithub } from 'react-icons/fa'
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href="https://linkedin.com" target="_blank"><BsLinkedin /></a>
-        <a href="https://github.com" target="_blank"><FaGithub /></a>
+        <a href="https://www.linkedin.com/in/salem-iyabode/" target="_blank"><BsLinkedin /></a>
+        <a href="https://github.com/hollowred" target="_blank"><FaGithub /></a>
     </div>
   )
 }
