2e5b23d78c47fbfc38929b5a692d15cdd0835579
Salem Iyabode <Shelton@Sheltons-MacBook-Pro.local>
2022-09-11 14:51:37 -0500

    contact complete

--- react-portfolio/src/components/contact/contact.jsx
+++ react-portfolio/src/components/contact/contact.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
 import './contact.css'
 import {AiOutlineMail} from 'react-icons/ai'
+import { useRef } from 'react';
+import emailjs from 'emailjs-com'
+
 
 const Contact = () => {
+  const form = useRef()
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_ds67cvw', 'template_o1i1t3n', form.current, 'Lqeie9VyCces4aNRI')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+
+      e.target.reset()
+  };
+
   return (
     <section id="contact">
       <h5>Get In Touch</h5>
@@ -11,15 +29,20 @@ const Contact = () => {
       <div className="container contact__container">
         <div className="contact__options">
           <article className="contact__option">
-            <AiOutlineMail />
+            <AiOutlineMail className='contact__option-icon'/>
             <h4>Email</h4>
             <h5>salemiyabode@gmail.com</h5>
-            <a href="mailto:salemiyabode@gmail.com">Send a message</a>
+            <a href="mailto:salemiyabode@gmail.com" target="_blank">Send a message</a>
           </article>  
 
         </div> 
         {/* END OF CONTACT OPTIONS   */}
-        <form action=""></form>
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name="name" placeholder="Full Name" required />
+          <input type="email" name="email" placeholder="Email" required />
+          <textarea name="message" rows="7" placeholder="Message" required ></textarea>
+          <button type ='submit' className='btn btn-primary'>Send Message</button>
+        </form>
       </div>  
 
 
