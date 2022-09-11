c8817670b5278af21033011a2227f2bbcee5f425
Salem Iyabode <Shelton@Sheltons-MacBook-Pro.local>
2022-09-10 19:44:29 -0500

    headers complete

--- react-portfolio/src/components/header/Header.jsx
+++ react-portfolio/src/components/header/Header.jsx
@@ -7,7 +7,7 @@ import HeaderSocial from './HeaderSocials'
 const Header = () => {
   return (
     <header>
-     <div className="container header_container">
+     <div className="container header__container">
         <h5>Hello I'm </h5>
         <h1>Salem Flagg</h1>
         <h5 className="text-light">Fullstack Developer</h5> 
@@ -18,7 +18,7 @@ const Header = () => {
           <img src={ME} alt="me" />
         </div>  
 
-        <a href="#contact" className='scroll_down'>Scroll Down</a>
+        <a href="#contact" className='scroll__down'>Scroll Down</a>
      </div>
     </header>
   )
