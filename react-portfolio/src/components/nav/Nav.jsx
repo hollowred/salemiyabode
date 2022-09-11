e5e3652dffc1876572169dec395f45b19eec5e4e
Salem Iyabode <Shelton@Sheltons-MacBook-Pro.local>
2022-09-11 08:40:44 -0500

    working on active nav

--- react-portfolio/src/components/nav/Nav.jsx
+++ react-portfolio/src/components/nav/Nav.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import './nav.css'
+import {AiFillHome, AiOutlineUser} from 'react-icons/ai'
+import {BiBookAlt, BiMessageDots} from 'react-icons/bi'
+import {FaSuitcase} from 'react-icons/fa'
+import {useState} from 'react'
+
+
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#')
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiFillHome /></a>
+      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser /></a>
+      <a href="#experience"><BiBookAlt /></a>
+      <a href="#portfolio"><FaSuitcase /></a>
+      <a href="#contact"><BiMessageDots/></a>
+    </nav>  
   )
 }
 
